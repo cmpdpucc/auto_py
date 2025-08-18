4d1b675ac641e884716cc40536018f732d2d79a8
dany.pu95 <dany.pu95@gmail.com>
2025-08-18 10:33:23 +0000

    Changes before Firebase Studio auto-run

--- flake.nix
+++ flake.nix
@@ -1,12 +1,27 @@
-nix
+
 {
-  description = "A basic Python development environment using .idx/dev.nix";
+  description = "A simple Python development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Or the channel you prefer
   };
 
-  outputs = { self, nixpkgs }: {
-    devShells.default = nixpkgs.legacyPackages.${nixpkgs.system}.callPackage ./.idx/dev.nix {};
-  };
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.${nixpkgs.system};
+      devNix = import ./.idx/dev.nix { inherit pkgs; };
+    in
+    {
+      devShells.${nixpkgs.system}.default = pkgs.mkShell {
+        packages = devNix.packages;
+        shellHook = ''
+          # Add any shell-specific configurations here
+        '';
+        # You can also include env variables from devNix.env here if needed
+        # e.g., nativeBuildInputs = [ pkgs.makeWrapper ];
+        #         postShellHook = ''
+        #           wrapProgram $EDITOR --set someVar someValue
+        #         '';
+      };
+    };
 }
